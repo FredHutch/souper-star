c3ab100fd4e9710866376080e3696eb953ea8a16
Sam Minot <sminot@fredhutch.org>
2022-11-14 13:34:11 -0800

    Fix BED file format

--- bin/make_bed.sh
+++ bin/make_bed.sh
@@ -3,4 +3,4 @@
 set -e
 
 samtools view "${1}" \
-    | perl -nle '@reads=split(/\t/,$_); if (m/CB:Z:([^\t\n]+)\t/) { print "$reads[2]\t","$reads[3]\t","$reads[3]\t",$1; }'
+    | perl -nle '@reads=split(/\t/,$_); if (m/CB:Z:([^\t\n]+)\t/) { print "$reads[2]\t","$reads[3]\t","$reads[7]\t",$1,"\t1"; }'
