995b6cded9c431b4e8233f099d3e6f52554b7f38
Jacob Greene <96763352+jacob-greene@users.noreply.github.com>
2022-11-18 15:35:38 -0800

    fixed bed start & end

--- bin/make_bed.sh
+++ bin/make_bed.sh
@@ -3,5 +3,5 @@
 set -e
 
 samtools view "${1}" \
-    | perl -nle '@reads=split(/\t/,$_); if (m/CB:Z:([^\t\n]+)\t/) { print "$reads[2]\t","$reads[3]\t","$reads[7]\t",$1; }' \
+    | perl -nle '@reads=split(/\t/,$_); if (m/CB:Z:([^\t\n]+)\t/) {$start=$reads[3]-1; $end=$reads[3]+length($reads[9])-1; print "$reads[2]\t","$start\t","$end\t",$1; }' \
     | bgzip -c
