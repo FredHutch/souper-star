06e680df5ca054f349f0d3beb567272d2b1783f5
Jacob Greene <96763352+jacob-greene@users.noreply.github.com>
2023-07-30 20:13:40 -0700

    Update make_bed.sh

--- bin/make_bed.sh
+++ bin/make_bed.sh
@@ -3,5 +3,5 @@
 set -e
 
 samtools view "${1}" \
-    | perl -nle '@reads = split(/\t/, $_); if (m/CB:Z:([^\t\n]+)\t/) {$start = $reads[3]-1; $reads_length = length($reads[9]); $TLEN = $reads[8]; if ($TLEN >= 0) {$end = $reads[3] + abs($TLEN) - 1;} else {$end = $reads[3] + $reads_length - 1;} print "$reads[2]\t$start\t$end\t$1";}' \
+    | perl -nle '@reads = split(/\t/, $_); if ((m/CB:Z:([^\t\n]+)\t/) && ($reads[1] & 64)) {$start = $reads[3] - 1; $reads_length = length($reads[9]); $TLEN = $reads[8]; if ($TLEN >= 0) {$end = $reads[3] + abs($TLEN) - 1;} else {$end = $reads[3] + $reads_length - 1;} print "$reads[2]\t$start\t$end\t$1";}' \
     | bgzip -c
