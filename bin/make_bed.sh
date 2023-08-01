1845919ff92f3525df310f8dc1667ed7c14eed2f
DerekJanssens <141066869+DerekJanssens@users.noreply.github.com>
2023-07-31 17:42:07 -0700

    Update make_bed.sh

--- bin/make_bed.sh
+++ bin/make_bed.sh
@@ -3,5 +3,16 @@
 set -e
 
 samtools view "${1}" \
-    | perl -nle '@reads = split(/\t/, $_); if ((m/CB:Z:([^\t\n]+)\t/) && ($reads[1] & 64)) {$start = $reads[3] - 1; $reads_length = length($reads[9]); $TLEN = $reads[8]; if ($TLEN >= 0) {$end = $reads[3] + abs($TLEN) - 1;} else {$end = $reads[3] + $reads_length - 1;} print "$reads[2]\t$start\t$end\t$1";}' \
+    | perl -nle '
+        @reads = split(/\t/, $_); 
+        if ((m/CB:Z:([^\t\n]+)\t/)) {
+            $reads_length = length($reads[9]);
+            $TLEN = $reads[8];
+            # Check if the read is aligned
+            if (($reads[1] & 4) == 0 && $TLEN >= 0) {
+                $start = $reads[3] - 1;
+                $end = $reads[3] + abs($TLEN) - 1;
+                print "$reads[2]\t$start\t$end\t$1";
+            } 
+        }' \
     | bgzip -c
