ac788c8cc0ccdc92922f672d7ee412b2bbae0c99
DerekJanssens <141066869+DerekJanssens@users.noreply.github.com>
2023-08-03 10:37:57 -0700

    Update make_bed.sh

--- bin/make_bed.sh
+++ bin/make_bed.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
-set -e
-
-samtools view "${1}" \
-    | perl -nle '
-        @reads = split(/\t/, $_); 
-        if ((m/CB:Z:([^\t\n]+)\t/)) {
-            $reads_length = length($reads[9]);
-            $TLEN = $reads[8];
-            # Check if the read is aligned
-            if (($reads[1] & 4) == 0 && $TLEN >= 0) {
-                $start = $reads[3] - 1;
-                $end = $reads[3] + abs($TLEN) - 1;
-                print "$reads[2]\t$start\t$end\t$1";
-            } 
-        }' \
+# Modify the QNAME to include the barcode as a comment, then convert to BED
+# Extract chromosome, start, end, and barcode and write to compressed output BED file
+samtools view -h "$1" \
+    | awk -v OFS='\t' '{
+        if ($0 ~ /^@/) {
+            print $0
+            next
+        }
+        split($1, arr, "_")
+        barcode = arr[2]
+        # Add the barcode as a comment in the QNAME
+        print $1 "#CB:" barcode, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15
+      }' \
+    | samtools view -b -h - \
+    | bedtools bamtobed -bedpe -i stdin \
+    | awk -v OFS='\t' '{
+        # Extract barcode from QNAME
+        split($4, arr, "#CB:")
+        split(arr[2], barcode_arr, "_")
+        barcode = barcode_arr[1]
+        # Print chromosome, start, end, and barcode
+        print $1, $2, $6, barcode
+      }' \
+    | sort -k1,1 -k2n,2n -k3n,3n \
     | bgzip -c
