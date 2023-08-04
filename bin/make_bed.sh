b21ece318b4bdecf59f821869b1cab9d3ae91e4b
DerekJanssens <141066869+DerekJanssens@users.noreply.github.com>
2023-08-03 20:04:38 -0700

    Update make_bed.sh

--- bin/make_bed.sh
+++ bin/make_bed.sh
@@ -2,11 +2,19 @@
 
 set -e
 
-#!/bin/bash
+#module load BEDTools/2.30.0-GCC-12.2.0 
+#module load SAMtools/1.17-GCC-12.2.0
 
-samtools sort -n "$1" | \
-  samtools view -h | \
-  bedtools bamtobed -bedpe -i stdin | \
-  cut -f1,2,6,7 | \
-  bgzip -c
+samtools sort -h "$1"  | \
+bedtools bamtobed -bedpe -i stdin | \
+perl -nle 'BEGIN { $header=1 }
+if ($header and /^@/) { print; next }
+if ($header and !/^@/) { $header=0 }
+@fields = split(/\t/);
+if ($fields[6] =~ m/CB:Z:([^\t\n]+)\t/) {
+  $barcode = $1;
+  print join("\t", $fields[0], $fields[1], $fields[5], $barcode);
+}' | \
+sort -k1,1 -k2n,2n -k3n,3n | \
+bgzip -c
 
