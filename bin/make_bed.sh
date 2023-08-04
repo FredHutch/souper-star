333d23ad9343ea1bb2cbb19a3f289bccc37bca62
DerekJanssens <141066869+DerekJanssens@users.noreply.github.com>
2023-08-03 19:53:20 -0700

    Update make_bed.sh

--- bin/make_bed.sh
+++ bin/make_bed.sh
@@ -4,4 +4,9 @@ set -e
 
 #!/bin/bash
 
-samtools view -h "$1"  | bedtools bamtobed -bedpe -i stdin | cut -f1,2,6,7 | sort -k1,1 -k2n,2n -k3n,3n | bgzip -c
+samtools sort -n "$1" | \
+  samtools view -h | \
+  bedtools bamtobed -bedpe -i stdin | \
+  cut -f1,2,6,7 | \
+  bgzip -c
+
