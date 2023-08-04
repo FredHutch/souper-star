c11183a736f4be099535feb48ece36ea6700d592
DerekJanssens <141066869+DerekJanssens@users.noreply.github.com>
2023-08-03 22:25:58 -0700

    Update make_bed.sh

--- bin/make_bed.sh
+++ bin/make_bed.sh
@@ -2,31 +2,22 @@
 
 set -e
 
-# Debug file
-debug_file="/home/djanssen/henikoff/for_dj/DJ_Hs_CD34_Exp123/souper-star_CSCB_D1-6_500reads_080323/debug_output.txt"
+# Extract filename without extension to use in the intermediate output file path
+filename=$(basename -- "$1")
+filename_no_ext="${filename%.*}"
 
-# Sort the SAM file by read name
 samtools sort -n "$1" | \
-# Convert to BAM and add modified read name with barcode from CB tag
 samtools view -h | \
-awk -F'\t' -v debug_file="$debug_file" '{
+awk -F'\t' '{
     if ($0 ~ /^@/) { print; next }
     if (match($0, /CB:Z:([^\t\n]+)\t/, arr)) {
-        print "Original: " $1 >> debug_file;  # Debug: print original read name
         split($1, a, "_");
-        print "First part: " a[1] >> debug_file;  # Debug: print first part of read name
-        print "Barcode: " arr[1] >> debug_file;  # Debug: print barcode
         $1 = a[1] "_" arr[1];
-        print "Modified: " $1 >> debug_file;  # Debug: print modified read name
     }
     print $0
-}' | \
-samtools view -b | \
-# Convert to BEDPE format
-bedtools bamtobed -bedpe -i stdin | \
-cut -f1,2,6,7 | \
-sort -k1,1 -k2n,2n -k3n,3n | \
-bgzip -c
+}' > "/home/djanssen/henikoff/for_dj/DJ_Hs_CD34_Exp123/souper-star_CSCB_D1-6_500reads_080323/intermediate_output_${filename_no_ext}.txt"
+
+
 
 
 
