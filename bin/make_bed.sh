e3e88beb9640af9683ab566bbbf4aba1d500f237
DerekJanssens <141066869+DerekJanssens@users.noreply.github.com>
2023-08-03 22:35:14 -0700

    Update make_bed.sh

--- bin/make_bed.sh
+++ bin/make_bed.sh
@@ -13,10 +13,14 @@ awk -F'\t' '{
     if (match($0, /CB:Z:([^\t\n]+)\t/, arr)) {
         split($1, a, "_");
         $1 = a[1] "_" arr[1];
+        # Rebuild $0
+        $0 = $1;
+        for(i=2; i<=NF; i++) {
+            $0 = $0 FS $i;
+        }
     }
     print $0
 }' > "/home/djanssen/henikoff/for_dj/DJ_Hs_CD34_Exp123/souper-star_CSCB_D1-6_500reads_080323/intermediate_output_${filename_no_ext}.txt"
-
 
 
 
