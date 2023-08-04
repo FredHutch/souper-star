7c1377f5fdf828feb4d7fd4de881ebfcbecc1aa3
DerekJanssens <141066869+DerekJanssens@users.noreply.github.com>
2023-08-03 21:09:18 -0700

    Update make_bed.sh

--- bin/make_bed.sh
+++ bin/make_bed.sh
@@ -1,37 +1,22 @@
 #!/bin/bash
 
-# Check if the input file is provided
-if [ -z "$1" ]; then
-    echo "Please provide a BAM file as input."
-    exit 1
-fi
+set -e
 
 samtools sort -n "$1" | \
 samtools view -h | \
-perl -nle '
-    @reads = split(/\t/, $_);
-
-    # If the line starts with "@", it's a header line, so just print it
-    if ($_ =~ /^@/) {
-        print;
-        next;
+awk -F'\t' '{
+    if ($0 ~ /^@/) { print; next }
+    match($0, /CB:Z:([^\t\n]+)\t/, arr)
+    if (arr[1] != "") {
+        sub(/_[A-Za-z0-9_]+$/, "_" arr[1], $1)
     }
+    print $0
+}' | \
+bedtools bamtobed -bedpe -i stdin | \
+cut -f1,2,6,7 | \
+sort -k1,1 -k2n,2n -k3n,3n | \
+bgzip -c
 
-    # Check for the specific pattern in the line
-    if (m/CB:Z:([^\t\n]+)\t/) {
-        # Store the value found in the pattern
-        $new_barcode = $1;
-
-        # Replace the barcode part of the read name with the new barcode
-        $reads[0] =~ s/_[A-Za-z0-9_]+$/_$new_barcode/;
-
-        # Join the modified fields back together and print
-        print join("\t", @reads);
-    } else {
-        # If the pattern is not found, print the line unchanged
-        print;
-    }
-'
 
 
 
