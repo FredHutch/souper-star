8909f39cd115529c416c92c5665b28e9eeedab1f
DerekJanssens <141066869+DerekJanssens@users.noreply.github.com>
2023-08-03 21:55:06 -0700

    Update make_bed.sh

--- bin/make_bed.sh
+++ bin/make_bed.sh
@@ -2,20 +2,25 @@
 
 set -e
 
+# Sort the SAM file by read name
 samtools sort -n "$1" | \
+# Convert to BAM and add modified read name with barcode from CB tag
 samtools view -h | \
 awk -F'\t' '{
     if ($0 ~ /^@/) { print; next }
-    match($0, /CB:Z:([^\t\n]+)\t/, arr)
-    if (arr[1] != "") {
-        sub(/_[A-Za-z0-9_]+$/, "_" arr[1], $1)
+    if (match($0, /CB:Z:([^\t\n]+)\t/, arr)) {
+        split($1, a, "_");
+        $1 = a[1] "_" arr[1];
     }
     print $0
 }' | \
+samtools view -b | \
+# Convert to BEDPE format
 bedtools bamtobed -bedpe -i stdin | \
 cut -f1,2,6,7 | \
 sort -k1,1 -k2n,2n -k3n,3n | \
 bgzip -c
+
 
 
 
