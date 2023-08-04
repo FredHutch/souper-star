7e1cb690fd568a7e64ceb6cef8cb2a6a69991ed6
DerekJanssens <141066869+DerekJanssens@users.noreply.github.com>
2023-08-03 20:56:18 -0700

    Update make_bed.sh

--- bin/make_bed.sh
+++ bin/make_bed.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-set -e
+# Check if the input file is provided
+if [ -z "$1" ]; then
+    echo "Please provide a BAM file as input."
+    exit 1
+fi
 
-# Perform sorting by read name and pipe into Perl for processing
 samtools sort -n "$1" | \
 samtools view -h | \
 perl -nle '
     @reads = split(/\t/, $_);
 
-    # If the line starts with '@', it's a header line, so just print it
+    # If the line starts with "@", it's a header line, so just print it
     if ($_ =~ /^@/) {
         print;
         next;
@@ -16,7 +19,7 @@ perl -nle '
 
     # Check for the specific pattern in the line
     if (m/CB:Z:([^\t\n]+)\t/) {
-        # Store the value found in the pattern (e.g., "GGTCATTTACTTGTTGATTACTCGCCTATCCT-18")
+        # Store the value found in the pattern
         $new_barcode = $1;
 
         # Replace the barcode part of the read name with the new barcode
@@ -25,14 +28,11 @@ perl -nle '
         # Join the modified fields back together and print
         print join("\t", @reads);
     } else {
-        # If pattern not found, print line unchanged
+        # If the pattern is not found, print the line unchanged
         print;
     }
-' |  bedtools bamtobed -bedpe -i stdin | \
-  cut -f1,2,6,7 | \
-sort -k1,1 -k2n,2n -k3n,3n | \
+'
 
-  bgzip -c
 
 
 
