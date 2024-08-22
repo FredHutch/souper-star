c237c591151e1dbc6ea9070fbc0de2bd2a09484b
Jacob Greene <96763352+jacob-greene@users.noreply.github.com>
2024-08-22 09:40:06 -0700

    Update make_bed.sh

--- bin/make_bed.sh
+++ bin/make_bed.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-samtools view "${1}" \
+samtools view -B -h "${1}" \
     | bedtools bamtobed -bedpe -i stdin | cut -f1,2,6,7 | sort -k1,1 -k2n,2n -k3n,3n | awk -v OFS='\t' '{len = $3 - $2 ; print $0, len }' \
     | bgzip -c
