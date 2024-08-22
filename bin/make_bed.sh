8d9027cf2669e3881496530bdf04cfb79716bf7c
Jacob Greene <96763352+jacob-greene@users.noreply.github.com>
2024-08-21 23:08:28 -0700

    Update make_bed.sh
    missing pipe

--- bin/make_bed.sh
+++ bin/make_bed.sh
@@ -3,5 +3,5 @@
 set -e
 
 samtools view "${1}" \
-    bedtools bamtobed -bedpe -i stdin | cut -f1,2,6,7 | sort -k1,1 -k2n,2n -k3n,3n | awk -v OFS='\t' '{len = $3 - $2 ; print $0, len }' \
+    | bedtools bamtobed -bedpe -i stdin | cut -f1,2,6,7 | sort -k1,1 -k2n,2n -k3n,3n | awk -v OFS='\t' '{len = $3 - $2 ; print $0, len }' \
     | bgzip -c
