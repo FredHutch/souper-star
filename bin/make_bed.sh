faabb8f014a3b512f9eb2dc202efac2f3606ca55
Jacob Greene <96763352+jacob-greene@users.noreply.github.com>
2024-08-22 09:57:06 -0700

    Update make_bed.sh
    switched back to perl for barcode processing

--- bin/make_bed.sh
+++ bin/make_bed.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-samtools view -B -h "${1}" \
-    | bedtools bamtobed -bedpe -i stdin | cut -f1,2,6,7 | sort -k1,1 -k2n,2n -k3n,3n | awk -v OFS='\t' '{len = $3 - $2 ; print $0, len }' \
+samtools view -F 0x900 "${1}" \
+    | perl -nle '@reads=split(/\t/,$_); if ($_ =~ /CB:Z:([^\t\n]+)/ && $reads[8] > 0) { $start=$reads[3]-1; $end=$start + $reads[8]; print "$reads[2]\t$start\t$end\t$1"; }' \
     | bgzip -c
