db2c6444d88e85b9fff74aaca8984309b1ad4f07
Jacob Greene <96763352+jacob-greene@users.noreply.github.com>
2024-08-22 16:02:20 -0700

    Update make_bed.sh
    Added cores

--- bin/make_bed.sh
+++ bin/make_bed.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-
+CORES=${1}
+BAM_IN="${2}"
 set -e
 
-samtools sort -n -m 2G -@ ${task.cpus} "${1}" \
+samtools sort -n -m 2G -@ ${CORES} "${BAM_IN}" \
 	| samtools view -B -h - \
 	| bedtools bamtobed -bedpe -i stdin | cut -f1,2,6,7 | sort -k1,1 -k2n,2n -k3n,3n | awk -v OFS='\t' '{len = $3 - $2 ; print $0, len }' \
 	| bgzip -c
