7e32394798aa9fa4a76004a4187613dec0ece5cd
Jacob Greene <96763352+jacob-greene@users.noreply.github.com>
2024-08-22 22:12:40 -0700

    Update make_bed.sh
    fix read lengths

--- bin/make_bed.sh
+++ bin/make_bed.sh
@@ -3,5 +3,5 @@
 set -e
 
 samtools view "${1}" \
-    | perl -nle '@reads=split(/\t/,$_); if (m/CB:Z:([^\t\n]+)\t/) {$start=$reads[3]-1; $end=$reads[3]+length($reads[9])-1; print "$reads[2]\t","$start\t","$end\t",$1; }' \
-    | bgzip -c
+    | perl -nle '@reads=split(/\t/,$_); if ($_ =~ /CB:Z:([^\t\n]+)/ && $reads[8] > 0) { $start=$reads[3]-1; $end=$start + $reads[8]; print "$reads[2]\t$start\t$end\t$1"; }' \
+    | sort -k1,1 -k2n,2n -k3n,3n | bgzip -c 
