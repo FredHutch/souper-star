225981e9f3a60dbe19fe5dae5b20b493910f98c3
Jacob Greene <96763352+jacob-greene@users.noreply.github.com>
2024-08-22 18:09:12 -0700

    Update make_bed.sh
    syntax

--- bin/make_bed.sh
+++ bin/make_bed.sh
@@ -6,4 +6,4 @@ set -e
 
 samtools view -B -h -f 0x40 "${BAM_IN}" \
     | perl -nle '@reads=split(/\t/,$_); if ($_ =~ /CB:Z:([^\t\n]+)/ && $reads[8] > 0) { $start=$reads[3]-1; $end=$start + $reads[8]; print "$reads[2]\t$start\t$end\t$1"; }' \
-    | LC_COLLATE=C sort -k1,1 -k2n,2n -k3n,3n | bgzip -c | zcat | head
+    | LC_COLLATE=C sort -k1,1 -k2n,2n -k3n,3n | bgzip -c
